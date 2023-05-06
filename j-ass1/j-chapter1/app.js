eab73740e132cde23f23cf6e8309ed58bbe961d1
sheikhwarisha <125816138+sheikhwarisha@users.noreply.github.com>
2023-05-06 09:44:47 -0700

    Add files via upload

---
+++ j-ass1/j-chapter1/app.js
@@ -0,0 +1,12 @@
+// ------Question no 1----------
+// alert("Hello ! Please visit my Professional Website ");
+// ------Question no 2----------
+// alert(" JavaScript Alert \n \n Error ! Please enter a valid password");
+// ------Question no 3----------
+// alert(" JavaScript Alert \n \n Welcome to JS Land \n Happy Coding! ");
+// ------Question no 4----------
+// alert(" JavaScript Alert \n \n Welcome to JS Land");
+// ------Question no 5----------
+// alert(" JavaScript Alert \n \n Hello .. I can run JS through my web browser's console");
+// ------Question no 6----------
+//  alert("It is My New HTML and CSS Project");
---
+++ j-ass1/j-chapter2/app1.js
@@ -0,0 +1,35 @@
+// -----Question no 01--------
+// let userName;
+// -----Question no 02--------
+// let myName = "Warisha Faiz";
+// -----Question no 03--------
+// let showMessage = "Hello World";
+// alert(showMessage);
+// -----Question no 04--------
+// let studentName ="Warisha Faiz";
+// let studentAge ="15 years old"; 
+// let studentCertificate ="Certified Mobile Application Development" ;
+// alert(studentName);
+// alert(studentAge);
+// alert(studentCertificate);
+// -----Question no 05--------
+// alert(" Pizza \n Pizz \n Piz \n Pi \n P")
+// for(let i=5; i>=1; i--){
+//     for(let j=1; j<=i; j++){
+//         alert("Pizza");
+//     }
+//     alert("<br>");
+// }
+// -----Question no 06--------
+// let email ="sheikhwarisha864@gmail.com";
+// alert("My email address is " + email);
+// -----Question no 07--------
+// let book ="A smarter way to learn JavaScript";
+//  alert("I am trying to learn from the book " + book);
+// -----Question no 08--------
+// document.write("Yah! I can write HTML content through JavaScript");
+// -----Question no 09--------
+// let design ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+// document.write(design);
+
