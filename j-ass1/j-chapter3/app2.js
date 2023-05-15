95ba5335156b0f8eafc48d549ac8735c2bef1631
sheikhwarisha <125816138+sheikhwarisha@users.noreply.github.com>
2023-05-15 11:40:23 -0700

    Add files via upload

---
+++ j-ass1/j-chapter3/app2.js
@@ -0,0 +1,21 @@
+// Question no 1
+// let age = +"15";
+// alert("I am " +age +" years old");
+// Question no 2
+// let number_Of_Times = +"14";
+// alert("You have visited this sites " +number_Of_Times +" times");
+// Question no 3
+//  let birth_Year = +"1990";
+//  document.writeln("My birth year is " +birth_Year);
+//  document.writeln(" <br>" );
+//  document. writeln(" Data type of my declared variable is " +typeof (birth_Year)) ;
+// Question no 4
+// let visitors_Name = "JOHN DOE";
+// let product_Title = "T-shirt(s)";
+// let quantity ="5";
+// document.write(  visitors_Name +" ordered " +quantity +product_Title +" on XYZ Clothing store");
+
+
+
+
+
