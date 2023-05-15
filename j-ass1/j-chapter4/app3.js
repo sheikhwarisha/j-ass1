61c1257692dba981e6d8fc8723c5ba69a6ce0e84
sheikhwarisha <125816138+sheikhwarisha@users.noreply.github.com>
2023-05-15 11:42:15 -0700

    Add files via upload

---
+++ j-ass1/j-chapter4/app3.js
@@ -0,0 +1,48 @@
+//--------- Question no 1
+// let first_Name ="Warisha";
+// let last_Name ="Faiz";
+// let age = +"20";
+// console.log( " My name is " +first_Name +" " +last_Name +" and my age is "+age);
+// ----------Question no 2
+// LEGAL VARIABLES
+// let myVar ;
+// let $name;
+// let last_Name;
+// let _student;
+// let number;
+//---------- Question no 3
+// ILLEGAL VARIABLES
+// let function;
+// let 1myage;
+// let today-Date;
+// let student name;
+// let (day;
+//---------- Question no 4
+// let num ="numbers";
+// let symbol1 ="$";
+// let symbol2 ="_";
+// let letter="letter";
+// let sensitivity ="sensitive";
+// let data ="keywords";
+// document.write(" <b> Rules for naming JS variables ");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("  Variable names can only contain " +num +" , " +symbol1 +" or " +symbol2 +" .  For example $my_1stVariable ");
+// document.write("<br>");
+// document.write("   Variables must begin with a  " +letter +" , " +symbol1 +" or " +symbol2 +" .  For example $name, _name or name ");
+// document.write("<br>");
+// document.write("    Variable names are case   " + sensitivity +" . ");
+// document.write("<br>");
+// document.write("  Variable names should not be JS    " + data +" . ");
+
+
+
+
+
+
+
+
+
+
+
+
