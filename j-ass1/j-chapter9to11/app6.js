1b93324d0bd1d75d36b2ac450ce842bed07f6079
sheikhwarisha <125816138+sheikhwarisha@users.noreply.github.com>
2023-05-15 11:44:52 -0700

    Add files via upload

---
+++ j-ass1/j-chapter9to11/app6.js
@@ -0,0 +1,156 @@
+//--------- Question no 1
+// let city_name = prompt("Enter your city name");
+// if(city_name == "Karachi" ){
+//     alert("“Welcome to city of lights” ");
+// }
+// else{
+//     console.log("Sorry try again");
+// }
+//--------- Question no 2
+// let gender = prompt("Enter your gender male or female :");
+// if(gender == "male"){
+//     console.log("Good Morning Sir");
+// }
+// else if( gender =="female"){
+//     console.log("Good Morning Mam");}
+// else
+//     console.log("Sorry try again");
+//--------- Question no 3
+// let color = prompt("Enter a traffic signal color Red ,Yellow or Green :");
+// if(color == "Red"){
+//     console.log("Must Stop");
+// }
+// else if( color =="Yellow"){
+//     console.log("Ready to move");}
+// else
+//     console.log("Move now");
+//--------- Question no 4
+// let fuel = prompt("Enter remaining fuel in car (in litres):");
+// if(fuel < 0.25){
+//     console.log("Please refill the fuel in your car");
+// }
+// else
+//     console.log("Don't need the fuel in your car");
+//--------- Question no 5
+// var a = 4; 
+// if (++a === 5)
+// { alert("given condition for variable a is true"); } 
+//Recorded true
+//------------
+// var b = 82;
+// if (b++ === 83)
+// { alert("given condition for variable b is true"); } 
+// var c = 12;
+//  if (c++ === 13)
+//  { alert("condition 1 is true"); }
+// if (c === 13)
+// { alert("condition 2 is true"); } 
+// if (++c < 14)
+// { alert("condition 3 is true"); } 
+// if(c === 14){ alert("condition 4 is true"); } 
+// Recorded condition 2 is true
+// Recorded condition 4 is true
+//------------
+// var materialCost = 20000; 
+// var laborCost = 2000; 
+// var totalCost = materialCost + laborCost;
+//  if (totalCost === laborCost + materialCost)
+//  { alert("The cost equals"); } 
+// Recorded true
+ //------------
+//  if (true)
+//  { alert("True"); }
+//   if (false)
+//   { alert("False"); } 
+// Recorded first condition is true
+ //------------
+// if("car" < "cat")
+// { alert("car is smaller than cat"); } 
+ // Recorded true
+ //--------- Question no 6
+// let obtain_marks1 = +prompt("Enter the obtain marks for subject1");
+// let obtain_marks2 = +prompt("Enter the obtain marks for subject2");
+// let obtain_marks3 = +prompt("Enter the obtain marks for subject3");
+// let total_marks = +prompt("enter total marks");
+// let obtain_marks = obtain_marks1 + obtain_marks2 + obtain_marks3;
+// let percentage = (obtain_marks * 100) / total_marks;
+// document.write("<br> MARK SHEET");
+// document.write("<br> <br> Total marks :",total_marks);
+// document.write("<br> Obtain marks :",obtain_marks);
+// document.write("<br> Percentage  :",percentage);
+// if(percentage >= 80){
+//     document.write( "<br> Grade: A-One");
+//     document.write( " <br> Remarks :Excellent");
+// }
+// else if(percentage >= 70){
+//     document.write( " <br> Grade A");
+//     document.write( " <br> Remarks :Good");
+// }
+// else if(percentage >= 60){
+//     document.write( " <br> Grade : B");
+//     document.write( " <br> You need to improve");
+// }
+// else if(percentage < 60){
+//     document.write( " <br> Grade : Fail");
+//     document.write( " <br> Sorry");
+// }
+ //--------- Question no 7
+//  let guess_number = +prompt("enter a number ranging from 1 to 10");
+//  let secret_number = 3 ;
+//  if (guess_number == secret_number){
+//     console.log(" Bingo ! Correct Answer");
+//  }
+//  else if ((guess_number + 1) == 5)
+//  {
+//     console.log(" Close enough to the correct answer");
+//  }
+//  else
+//  console.log(" Sorry");
+ //--------- Question no 8
+// let number = +prompt("please enter a number");
+// if(number/3 === 1)
+// {
+// console.log("the number is divisible by 3");
+// }
+// else
+//  console.log("the number is not  divisible by 3");
+ //--------- Question no 9
+// let number = +prompt("please enter a number");
+// if(number/2 === 1)
+// {
+// console.log("the number is Even");
+// }
+// else
+//  console.log("the number is Odd");
+ //--------- Question no 10
+//  let temperature = +prompt("please enter a temperature");
+// if( temperature > 40)
+// console.log("It is too hot outside.");
+// else if(temperature > 30)
+//  console.log("The Weather today is Normal.");
+//  else if(temperature > 20)
+//  console.log("Today’s Weather is cool.");
+//  else if(temperature > 10)
+//  console.log("OMG! Today’s weather is so Cool.");
+// else 
+// console.log("Invalid inpput.");
+ //--------- Question no 11
+// let number1 = +prompt("please enter a number1");
+// let number2 = +prompt("please enter a number2");
+// console.log(number1 +" + " +number2 +" is ",number1+number2);
+// console.log(number1 +" - " +number2 +" is ",number1-number2);
+// console.log(number1 +" * " +number2 +" is ",number1*number2);
+// console.log(number1 +" / " +number2 +" is ",number1/number2);
+// console.log(number1 +" % " +number2 +" is ",number1%number2);
+
+
+
+
+
+
+
+
+
+
+
+
