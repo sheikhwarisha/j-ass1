a2257d4f1cd54f78b00392526bb5452da72cf59a
sheikhwarisha <125816138+sheikhwarisha@users.noreply.github.com>
2023-05-15 11:43:14 -0700

    Add files via upload

---
+++ j-ass1/j-chapter5/app4.js
@@ -0,0 +1,159 @@
+//--------- Question no 1
+// var num1 = 2;
+// var num2 = 3;
+// var result = num1 + num2;
+// document.write("Sum of " +num1 +" and " +num2 +" is " +result);
+//--------- Question no 2
+//----FOR SUBTRACTION
+//  var num1 = 3;
+// var num2 = 2;
+//  var result = num1 - num2;
+// document.write("Sub of " +num1 +" and " +num2 +" is " +result);
+//----FOR MULTIPLICATION
+// var num1 = 3;
+// var num2 = 2;
+//  var result = num1 * num2;
+// document.write("Mul of " +num1 +" and " +num2 +" is " +result);
+//----FOR DIVISION
+// var num1 = 4;
+// var num2 = 2;
+//  var result = num1 / num2;
+// document.write("Divide of " +num1 +" and " +num2 +" is " +result);
+//----FOR REMINDER
+// var num1 = 4;
+// var num2 = 2;
+//  var result = num1 % num2;
+// document.write("Reminder of " +num1 +" and " +num2 +" is " +result);
+//--------- Question no 3
+// var num;
+// document.write("Value after variable declaration is : " +num);
+// document.write("<br>");
+// num = "5";
+// document.write("Initial value: " +num);
+// num =++num;
+// document.write("<br>");
+// document.write("Value after increment is: " +num);
+// num = 7 + num;
+// document.write("<br>");
+// document.write("Value after addition is : " +num);
+// num =--num;
+// document.write("<br>");
+// document.write("Value after  decrement is: : " +num);
+// num = num % 3;
+// document.write("<br>");
+// document.write("The reminder is : " +num); 
+//--------- Question no 4
+// let ticket_price = 600 ;
+// let cost = ticket_price + ticket_price + ticket_price + ticket_price + ticket_price ;
+// document.write("Total cost to buy 5 tickets to a movie is "  +cost +"PKR");
+//--------- Question no 5
+// let num = +prompt("Please enter a number");
+// for (let i =1; i<=10 ;i++){
+//      document.write("<br>");
+//     document.write(num +" * " +i +" = "  +num*i);
+// }
+//--------- Question no 6
+// // celcius to farenhite
+// var celcius = +prompt("Please enter a temperature in to celcius");
+// var  farenhite = (celcius *(9/5)+32);
+// document.write(celcius +" <sup>o<sup>C is " +farenhite +" <sup>o<sup>F");
+//  farenhite to celcius 
+// var  farenhite = +prompt("Please enter a temperature in to farenhite");
+// var  celcius = ((farenhite -32) *(5/9));
+// document.write (farenhite +" <sup>o<sup>F is  " +celcius +" <sup>o<sup>C");
+//--------- Question no 7
+// let price_of_item1 = 650;
+// let price_of_item2 = 250;
+// let shipping_charges = 100 ;
+// let quantity1 = +prompt("Enter a quantity for item 1 :");
+// let quantity2 = +prompt("Enter a quantity for item 2 :");
+// let total_cost =  (price_of_item1 *quantity1) +(price_of_item2 *quantity2) +shipping_charges;
+// document.write(" <br> Price of item 1 is : " ,price_of_item1);
+// document.write(" <br> Quantity of item 1 is : " ,quantity1);
+// document.write("  <br> Price of item 1 is : " ,price_of_item2);
+// document.write(" <br> Quantity of item 1 is : " ,quantity2);
+// document.write(" <br> Shipping charges is : " ,shipping_charges);
+// document.write(" <br> Total cost  is : " ,total_cost);
+//--------- Question no 8
+// let total_marks = 980;
+// let obtained_marks = 804;
+// let percentage = (obtained_marks *100) / total_marks;
+// document.write(" <br> Total Marks is : " ,total_marks);
+// document.write(" <br> Obtain Marks  is : " ,obtained_marks);
+// document.write(" <br> Percentage is : " ,percentage);
+//--------- Question no 9
+// let dollars = 10;
+// let riyal = 25;
+// let total_currency = (dollars * 104.80) + (riyal * 28);
+// document.write(" <b> Currency in PKR <br> <br>");
+//  document.write(" <br> Total currency in PKR is : " ,total_currency);
+//--------- Question no 10
+// let num = 3;
+// let result = ((num +5) *10 ) /2;
+//--------- Question no 11
+// let current_year = 2016;
+// let birth_year = 1992;
+// let age1 = current_year - birth_year ;
+// document.write(" <br> Your current year is : " ,current_year);
+// document.write("<br> Your birth year is : " ,birth_year);
+// //---------------------------------------------
+// let current_Year = 2023;
+// let birth_Year = 2001;
+// let age2 = current_Year - birth_Year ;
+// document.write(" <br> Your current year is : " ,current_Year);
+// document.write("<br> Your birth year is : " ,birth_Year);
+// document.write("<br> They are either : " +age1 +" or " +age2 +" years old");
+//--------- Question no 12
+// let radius = 20;
+// let circumference = 2 * 3.142 * radius;
+// let area = 3.142 * radius * radius ;
+// document.write(" <br> Radius is : " ,radius);
+// document.write(" <br> Circumference  is : " ,circumference);
+// document.write(" <br> Area  is : " ,area);
+//--------- Question no 13
+// let fav_Snack = "lays" ;
+// let current_age = 15;
+// let max_age = 65;
+// let snack_per_day = 3;
+// let value = 0;
+// let value1 = (max_age - current_age)*365;
+// for (let i=1;i<=value1;i++){
+//      value = value+snack_per_day;
+// }
+// document.write("The Life Time Supply Calculator");
+// document.write("<br> Your favourite Snack : ",fav_Snack);
+// document.write(" <br> Your current age : ",current_age);
+// document.write(" <br> Your estimated max age : ",max_age);
+// document.write(" <br> amount of snack u eat per day : ",snack_per_day);
+// document.write(" <br> You will need : "+value +" " +fav_Snack +" to last you until the ripe old age of " +max_age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
