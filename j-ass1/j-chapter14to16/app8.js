f7668ee4dbea11accf90006b8fdfc73792bf4f20
sheikhwarisha <125816138+sheikhwarisha@users.noreply.github.com>
2023-06-05 11:56:51 -0700

    Add files via upload

---
+++ j-ass1/j-chapter14to16/app8.js
@@ -0,0 +1,140 @@
+// Question no 1
+// let studentname = [];
+// Question no 2
+// let studentname = [];
+// Question no 3
+// let name = ["Ali" , "Sara" ,"Mehnaz"];
+// Question no 4
+// let num = [1,2,,4,5,6,7];
+// Question no 5
+// let num = [true , false , true];
+// Question no 6
+// let arr = ["Ali" ,1 ,2,"3", null,true,false];
+// Question no 7
+// let education_qualifications = ["SSC" ,"HSC" ,"BSCS" ,"BCOM" ,"BS" ,"BCOM" ,"MS" ,"M.PHiL" ,"PhD"] ;
+// document.write("Qualifications: <br>");
+// document.write("<ol>");
+// document.write("<li>" +education_qualifications[0] +"</li>");
+// document.write("<li>" +education_qualifications[1] +"</li>");
+// document.write("<li>" +education_qualifications[2] +"</li>");
+// document.write("<li>" +education_qualifications[3] +"</li>");
+// document.write("<li>" +education_qualifications[4] +"</li>");
+// document.write("<li>" +education_qualifications[5] +"</li>");
+// document.write("<li>" +education_qualifications[6] +"</li>");
+// document.write("<li>" +education_qualifications[7] +"</li>");
+// document.write("<li>" +education_qualifications[8] +"</li>");
+// document.write("</ol>");
+// Question no 8
+// let studentname =["Aiman" ,"Wania" ,"Hania"];
+// let score =[ 209,479 , 199];
+// let total_marks =500;
+// percentage1 = ((score[0] *100) / total_marks) ;
+// percentage2 = ((score[1] *100) / total_marks) ;
+// percentage3 = ((score[2] *100) / total_marks) ;
+// document.write("Score of " ,studentname[0] +" is " ,score[0] +" Percentage: " ,percentage1);
+// document.write(" <br> Score of " ,studentname[1] +" is " ,score[1] +" Percentage: " ,percentage2);
+// document.write(" <br> Score of " ,studentname[2] +" is " ,score[2] +" Percentage: " ,percentage3);
+// Question no 9
+// var colors = ["red", "green", "blue"];
+// document.write(" " + colors + "<br>");
+// var color1 = prompt("Enter a color to add  the beginning of the array");
+// colors.unshift(color1);
+// document.write(" "  + colors + "<br>");
+// color1 = prompt("Enter a color to add to the end of the array");
+// colors.push(color1);
+// document.write(" " + colors + "<br>");
+// colors.unshift("yellow", "orange");
+// document.write(" " + colors + "<br>");
+// colors.shift();
+// document.write(" " + colors + "<br>");
+// colors.pop();
+// document.write(" " + colors + "<br>");
+// var index = prompt("Enter an index to add a color");
+// newColor = prompt("Enter a color to add");
+// colors.splice(index, 0, newColor);
+// document.write(" " + colors + "<br>");
+// index = prompt("Enter an index to start deleting colors");
+// var numColors = prompt("Enter the number of colors to delete");
+// colors.splice(index, numColors);
+// document.write(" " + colors + "<br>");
+
+// Question no 10
+// let arr =[320, 230 , 480, 120];
+// document.write("Scores of student : ",arr);
+// let arr2 =arr.sort();
+// document.write("<br>");
+// document.write("Ordered Scores of Student : ",arr2);
+// Question no 11
+// let city_Names =["Karachi" ,"Lahore" ," Islamabad" ,"Quetta" ,"Peshawar"];
+// document.write("Cities List : ",city_Names );
+// document.write(" <br>");
+// let selected_List = city_Names.slice(2,4);
+// document.write(" Selected cities List : ",selected_List);
+// Question no 12
+// var data = ["This" ,"is" ,"my" ,"cat"];
+// document.write("Array : " ,data);
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// var str = data.join(" ");
+// document.write("String : " ,str);
+// Question no 13
+// let array = [];
+// array.push("keyboard");
+// array.push("mouse");
+// array.push("printer");
+// array.push("monitor");
+// document.write(array);
+// document.write("<br>");
+// document.write(array.shift());
+// document.write("<br>");
+// document.write(array.shift());
+// document.write("<br>");
+// document.write(array.shift());
+// document.write("<br>");
+// document.write(array.shift());
+//Question no 14
+// let array = [];
+// array.push("keyboard");
+// array.push("mouse");
+// array.push("printer");
+// array.push("monitor");
+// document.write(array);
+// document.write("<br>");
+// document.write(array.pop());
+// document.write("<br>");
+// document.write(array.pop());
+// document.write("<br>");
+// document.write(array.pop());
+// document.write("<br>");
+// document.write(array.pop());
+//Question no 15
+// let phoneManufactures =[ "Apple" ,"Samsung" ,"Motrola" ,"Nokia" ,"Sony" ,"Haier"];
+// document.write("<select>");
+// for(var i = 0 ;i < phoneManufactures.length ;i++){
+// document.write("<option>" +phoneManufactures[i] +"</option>")
+// }
+// document.write("</select>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
