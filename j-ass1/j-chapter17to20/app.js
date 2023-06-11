a34db0890b1f6d468dfc142b1a42c09fd69c839f
sheikhwarisha <125816138+sheikhwarisha@users.noreply.github.com>
2023-06-11 10:18:09 -0700

    Add files via upload

---
+++ j-ass1/j-chapter17to20/app.js
@@ -0,0 +1,93 @@
+// Question no 1
+// let data = [[],[],[]];
+// Question no 2
+// let matrix = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+// Question no 3
+// for(let i = 1; i <= 10 ; i++ )
+// {
+//     document.write(i +"<br>");
+// }
+// Question no 4
+// let num = +prompt("please enter a number");
+// let length = +prompt("please enter a length");
+// document.write("Multiplication table of " +num +" length " +length +"<br>");
+// for (let i = 1; i <= length; i++){
+//     document.write(num +" X " +i +" = " +num*i +" <br> ");
+// }
+// Question no 5
+// let fruits =["apple","banana","mango","orange","stawberry"];
+// for(let i = 0;i <= 4; i++){
+//     document.write(fruits[i] +"<br>");
+// }
+// document.write("<br>");
+
+// for(let i = 0;i <= 4; i++){
+//     document.write("Elements at index " +i +" is " +fruits[i] +"<br>");
+// }
+// Question no 6
+// document.write("Counting: ");
+// for(let i = 1; i<=15 ; i++){
+// document.write(i+",");
+// }
+// document.write("<br> Reverse Counting: ");
+// for(let i = 10; i >=1 ; i--){
+//     document.write(i+",");
+//     }
+// document.write("<br> Even : ");
+// for(let i = 0; i <=20 ; i+= 2){
+//     document.write(i+",");
+//     }
+//  document.write("<br> Odd : ");
+//     for(let i = 1; i <=19 ; i+= 2){
+//         document.write(i+",");
+//         }
+// document.write("<br> Series : ");
+//         for(let i = 1; i <=20 ; i++){
+//             if (i%2 == 0) {
+//                 document.write(i+"k,");
+//             }
+//             }
+// Question no 7
+// let bakery_item = ["cake", "apple pie", "cookie", "chips", "patties"];
+// let user_item = prompt("Welcome to sawat bakery ,What do you want to order sir/mam ?") ;
+// let flag ="not found";
+// for(var i = 0 ;i < bakery_item.length ; i++){
+// if (user_item === bakery_item[i]) {
+//     flag = "found";
+// }
+// }
+// if (flag === "found") {
+//     document.write(" ",user_item +" is available in our bakery");
+// }
+// else    
+// document.write("we are sorry ",user_item +" is not available in our bakery" );
+// Question no 8
+// let num = [24,53,78,91,12]
+// let max = num[0]
+// for (let i = 1; i < num.length; i++) {
+//     if (num[i] > max) {
+//        max = num[i] 
+//     }  
+// }
+// document.write("Array Items :",num)
+// document.write( "<br> The largest number in this array is:",max);
+// Question no 9
+// let num = [24,53,78,91,12]
+// let max = num[0]
+// for (let i = 1; i < num.length; i++) {
+//     if (num[i] < max) {
+//        max = num[i] 
+//     }  
+// }
+// document.write("Array Items :",num)
+// document.write( "<br> The Smallest number in this array is:",max);
+// Question no 10
+// for(let i =1; i<=100; i++){
+//     if(i%5 == 0){
+//         document.write(i +",")
+//     }
+// }
+
+
+
+
